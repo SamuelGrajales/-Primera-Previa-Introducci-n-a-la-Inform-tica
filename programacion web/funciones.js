9d020a0a7ad5df45c209f9bf420cbf694cfcb50e
SamuelGrajales <89594307+SamuelGrajales@users.noreply.github.com>
2021-10-18 16:19:56 -0500

    Add files via upload

---
+++ programacion web/funciones.js
@@ -0,0 +1,44 @@
+document.write("NUMEROS BINARIOS<br>");
+document.write("--------------------------<br><br>");
+document.write("Numero: 1011 =");
+
+var n;
+n = 1 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write( n + "<br>");
+document.write("--------------------------<br><br>");
+
+document.write("Numero: 0000 =");
+
+var n;
+n = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 0 * (Math.pow(2,0));
+
+document.write( n + "<br>");
+
+document.write("Numero: 0001 =");
+
+var n;
+n = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 0 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write( n + "<br>");
+
+document.write("Numero: 0011 =");
+
+var n;
+n = 0 * (Math.pow(2,3)) + 0 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write( n + "<br>");
+
+document.write("Numero: 0111 =");
+
+var n;
+n = 0 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write( n + "<br>");
+
+document.write("Numero: 1111 =");
+
+var n;
+n = 1 * (Math.pow(2,3)) + 1 * (Math.pow(2,2)) + 1 * (Math.pow(2,1)) + 1 * (Math.pow(2,0));
+
+document.write( n + "<br>");
